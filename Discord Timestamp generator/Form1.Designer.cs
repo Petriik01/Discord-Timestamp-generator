64216a2f9f1dd3764a689e6f38676035dba82af8
Petriik01 <81877597+Petriik01@users.noreply.github.com>
2023-08-31 16:53:19 +0200

    basic functionality

--- Discord Timestamp generator/Form1.Designer.cs
+++ Discord Timestamp generator/Form1.Designer.cs
@@ -28,20 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(371, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "relative time (5 days ago)",
+            "short date time (May 5, 2022 10:59 PM)",
+            "long date time (Monday, May 5, 2022 10:59 PM)",
+            "short date\t(06/27/2021)",
+            "long date (May 5, 2022)",
+            "short time (10:59 PM)",
+            "long time (10:59:37 PM)"});
+            this.comboBox1.Location = new System.Drawing.Point(96, 144);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Type of time generated";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Date and time";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(75, 261);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(245, 20);
+            this.textBoxOutput.TabIndex = 13;
+            this.textBoxOutput.Text = "Your output will be there";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(96, 86);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(203, 20);
+            this.dateTimePicker2.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 355);
+            this.ClientSize = new System.Drawing.Size(395, 399);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Discord Timestamp Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
 
--- Discord Timestamp generator/Form1.cs
+++ Discord Timestamp generator/Form1.cs
@@ -12,9 +12,58 @@ namespace Discord_Timestamp_generator
 {
     public partial class Form1 : Form
     {
+        DateTime time;
+
+        string type;
+
+        DateTime date;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            time = dateTimePicker2.Value;
+
+
+            type = comboBox1.Text;
+
+            Int32 unixTimestamp = (int)time.Subtract(new DateTime(1970, 1, 1, 2, 0, 0)).TotalSeconds;
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    textBoxOutput.Text = "<t:" + unixTimestamp.ToString() + ":R>";
+                    break;
+                case 1:
+                    textBoxOutput.Text = "<t:" + unixTimestamp.ToString() + ":f>";
+                    break;
+                case 2:
+                    textBoxOutput.Text = "<t:" + unixTimestamp.ToString() + ":F>";
+                    break; 
+                case 3:
+                    textBoxOutput.Text = "<t:" + unixTimestamp.ToString() + ":d>";
+                    break;
+                case 4:
+                    textBoxOutput.Text = "<t:" + unixTimestamp.ToString() + ":D>";
+                    break;
+                case 5:
+                    textBoxOutput.Text = "<t:" + unixTimestamp.ToString() + ":t>";
+                    break;
+                case 6:
+                    textBoxOutput.Text = "<t:" + unixTimestamp.ToString() + ":T>";
+                    break;
+                default:
+                    textBoxOutput.Text = "Your output will be there";
+                    break;
+            }
+
+            
         }
     }
 }
